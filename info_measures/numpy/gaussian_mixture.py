3c4bca4afb0502503b310efa13e66551986c8f80
Jesse Livezey <jlivezey@lbl.gov>
2018-04-25 14:12:42 -0700

    mixture mi is working

--- info_measures/numpy/gaussian_mixture.py
+++ info_measures/numpy/gaussian_mixture.py
@@ -9,7 +9,7 @@ class GaussianMixture(object):
         self.center_var = center_var
         self.var = var
         self.rng = np.random.RandomState(seed)
-        self.center = self.rng.normal(scale=center_var, size=(n_mixtures, dim))
+        self.center = self.rng.normal(scale=np.sqrt(center_var), size=(n_mixtures, dim))
 
 
     def eval_p(self, X):
@@ -33,18 +33,39 @@ class GaussianMixture(object):
         n_mixtures = self.n_mixtures
         idxs = self.rng.multinomial(1, np.ones(n_mixtures)/n_mixtures, n_samples)
         idxs = idxs.argmax(axis=-1)
-        samples = self.rng.randn(n_samples, self.dim) * self.var
+        samples = self.rng.randn(n_samples, self.dim) * np.sqrt(self.var)
         samples += self.center[idxs]
         return samples
 
 
     def sample_entropy(self, n_samples):
         samples = self.sample(n_samples)
-        logp = self.eval_logp(samples)
-        return samples, -logp.dot(np.exp(logp)) / n_samples
+        return samples, -self.eval_logp(samples).mean()
 
 
     def sample_mutual_information(self, n_samples, var1_dim):
         assert var1_dim < self.dim
+        assert var1_dim > 0
         samples = self.sample(n_samples)
-        return
+        logp = self.eval_logp(samples)
+        Xlogp, Ylogpy = self.eval_logp_xy(samples, var1_dim)
+        return samples, (logp - Xlogp - Ylogpy).mean()
+
+
+    def eval_logp_xy(self, X, var1_dim):
+        assert X.ndim == 2
+        assert X.shape[1] == self.dim
+        Xp = X[:, np.newaxis, :var1_dim]
+        Xcenterp = self.center[np.newaxis, :, :var1_dim]
+        Yp = X[:, np.newaxis, var1_dim:]
+        Ycenterp = self.center[np.newaxis, :, var1_dim:]
+
+        var = self.var
+        Xmixture_ll = -(Xp - Xcenterp)**2 / 2. / var - .5*np.log(2. * np.pi * var)
+        Xmixture_ll = Xmixture_ll.sum(axis=-1)
+        Ymixture_ll = -(Yp - Ycenterp)**2 / 2. / var - .5*np.log(2. * np.pi * var)
+        Ymixture_ll = Ymixture_ll.sum(axis=-1)
+
+        Xlogp = logsumexp(Xmixture_ll, axis=-1) - np.log(self.n_mixtures)
+        Ylogp = logsumexp(Ymixture_ll, axis=-1) - np.log(self.n_mixtures)
+        return Xlogp, Ylogp
--- info_measures/numpy/tests/test_entropy.py
+++ info_measures/numpy/tests/test_entropy.py
@@ -2,23 +2,20 @@ import numpy as np
 from numpy import testing as npt
 from scipy.stats import (multivariate_normal, ttest_1samp)
 
-from science_nets.numpy import (kraskov_stoegbauer_grassberger as ksg,
-                                kolchinsky_tracey as kt)
+from info_measures.numpy import (kraskov_stoegbauer_grassberger as ksg,
+                                 kolchinsky_tracey as kt)
 
 
 def mvn_entropy(mean, covariance):
     return np.asscalar(multivariate_normal(mean, covariance).entropy())
 
 
-def test_mvn_entropy():
-    """Test entropy estimators on multivariate normal data."""
-    n_repeat = 100
-    n_samples = 10000
+def test_mvn_kt_entropy():
+    """Test Kolchinsky-Tracey entropy estimators on multivariate normal data."""
+    n_repeat = 10
     eu = np.zeros(n_repeat)
     el = np.zeros(n_repeat)
-    ek = np.zeros(n_repeat)
-    for dim in [1, 2, 3]:
-        n_samples = 500 * 10**(dim // 2)
+    for dim, n_samples in zip([1, 2, 3], [1000, 1000, 10000]):
         mu = np.zeros(dim)
         svar = np.diag(np.sqrt(np.power(10., np.arange(dim) - (dim-1.)/2.)))
         e = mvn_entropy(mu, svar)
@@ -26,15 +23,27 @@ def test_mvn_entropy():
         for ii in range(n_repeat):
             x = np.random.multivariate_normal(mu, svar, size=n_samples)
             kt_estimator = kt.Entropy(x)
-            ksg_estimator = ksg.Entropy(x)
             eu[ii] = kt_estimator.entropy(lower=False)
             el[ii] = kt_estimator.entropy(lower=True)
-            ek[ii] = ksg_estimator.entropy()
         npt.assert_array_less(e, eu, 'Entropy upper bound was not larger ' +
                                      'than ground truth. Dim: {}'.format(dim))
         npt.assert_array_less(0., el, 'Entropy lower bound was not larger ' +
                                       'than zero. Dim: {}'.format(dim))
         npt.assert_array_less(el, e, 'Entropy lower bound was not smaller ' +
                                      'than ground truth. Dim: {}'.format(dim))
-        npt.assert_array_less(abs(e-ek.mean()), ek.std(), 'k-nn entropy estimate was incorrect. ' +
+
+
+def test_mvn_ksg_entropy():
+    """Test Kraskov=Stoegbauer-Grassberger entropy estimator on multivariate normal data."""
+    n_repeat = 10
+    ek = np.zeros(n_repeat)
+    for dim, n_samples in zip([1, 2, 3], [1000, 1000, 10000]):
+        mu = np.zeros(dim)
+        svar = np.diag(np.sqrt(np.power(10., np.arange(dim) - (dim-1.)/2.)))
+        e = mvn_entropy(mu, svar)
+        for ii in range(n_repeat):
+            x = np.random.multivariate_normal(mu, svar, size=n_samples)
+            ksg_estimator = ksg.Entropy(x)
+            ek[ii] = ksg_estimator.entropy()
+        npt.assert_array_less(abs(e-ek.mean()), 2. * ek.std(), 'k-nn entropy estimate was incorrect. ' +
                                       'Dim: {}, {}, {}, {}'.format(dim, e, ek.mean(), ek))
